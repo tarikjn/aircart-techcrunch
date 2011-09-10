f72999eda4cb80458096ad23d78804406a2a884e
Tarik Ansari <tarikjn@gmail.com>
2011-09-10 16:04:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aircart::Application.config.secret_token = '5ccdf59c4309ee9964280666fb74f4027217da976f5069ee3923310f6b61c809a11ff39b72c5964f625341c76cc0f45c1b992289a0540acbd28cb6f6cedc18d7'
