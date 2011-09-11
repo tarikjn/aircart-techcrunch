cf0105ef15d13b85089fbb8c164a14aca16493c6
Tarik Ansari <tarikjn@gmail.com>
2011-09-11 01:01:23 -0700

    mobile fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ private
 
   def prepare_for_mobile
     session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
+    request.format = :mobile if mobile_device? and request.format != :json
   end
   
 end
