f2cc655bdcbe2232d2b2b17b7d999aa741f16729
Tarik Ansari <tarikjn@gmail.com>
2011-09-11 03:42:13 -0700

    better mobile/desk fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,6 @@ class HomeController < ApplicationController
   def index
     @cart = Cart.new
     @products = Product.find(:all, :limit => 20)
-    
-    # ugly hack
-    respond_to { |format|
-      format.any(:html, :mobile) {
-        render :template => "home/index.html.erb"
-      }
-    }
-    
   end
 
 end
